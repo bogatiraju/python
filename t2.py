b0b720d59ee3b5f5320f5ff3150a64068dce5009
bogatiraju <bogatiraju@gmail.com>
2023-10-13 22:59:20 +0545

    commit

---
+++ t2.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3]
+strings = ["a", "b", "c"]
+names = ["John", "Eric", "Jessica"]
+
+# Loop through the lists and print the elements
+// for number, string, name in zip(numbers, strings, names):
+    print(f"Number: {number}, String: {string}, Name: {name}")
---
+++ t3.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3]
+strings = ["a", "b", "c"]
+names = ["John", "Eric", "Jessica"]
+
+# Print the first elements from each list
+print(f"Number: {numbers[0]}, String: {strings[0]}, Name: {names[0]}")
